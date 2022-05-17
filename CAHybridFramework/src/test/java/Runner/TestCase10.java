2922815c25b557011bca42e56828728b9bdf3c04
Kokila K <kokilak1721998@gmail.com>
2022-05-17 23:37:44 +0530

    push

--- CAHybridFramework/src/test/java/Runner/TestCase10.java
+++ CAHybridFramework/src/test/java/Runner/TestCase10.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -59,7 +59,7 @@ public class TestCase10 extends BaseClass {
 		}
 	}
 
-	@AfterTest
+	@AfterSuite
 	public void teardown() {
 		driver.close();
 		driver = null;
